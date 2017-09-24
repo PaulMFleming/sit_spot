2ad571b2d7e236afb1205e51c61be671f4181bd6
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-09-24 19:34:06 +0100

    set up registration page

--- sit_spot_site/urls.py
+++ sit_spot_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from hello import views as hello_views
+from accounts import views as accounts_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', hello_views.get_index, name='index'),
+    url(r'^register/$', accounts_views.register, name='register'),
 ]
